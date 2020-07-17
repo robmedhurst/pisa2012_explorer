f71f4f9d51ebe0dfb50ce004ec87027b0206c48c
Robbie <robert.medhurst241@gmail.com>
2020-07-17 10:09:45 -0400

    Add framework for wrangling functions
    Wrangling process is on the page in sudo

--- pisaexplorer.py
+++ pisaexplorer.py
@@ -1,6 +1,9 @@
 # This tool aims to aid in the exploration of the PISA 2012 dataset,
 # allowing users to concurrently examine a group of similar variables.
 
+
+#%% Import Libraries
+
 import zipfile
 
 import numpy as np
@@ -8,7 +11,6 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 
 
 #%% Load CSVs
@@ -28,8 +30,7 @@ PISADICT2012 = pd.read_csv(
         columns={'Unnamed: 0':'varname', 'x': 'description'})
 
 
-
-#%% Define terms
+#%%TERMS
 
 ### Known Categories
 # Dictionary containing lists. Each list contains the values of known
@@ -52,18 +53,85 @@ independent_groups = {}
 dependent_group = {}
 
 
-#%% outline wrangling in shorthand
+#%% FUNCTIONS
 
-# df = a PISA sample or subset of interest
 
-# intialize(df, independent_groups, dependent_group)
+def initialize(pisa_df, indep_groups, dep_group):
+    """
+    general wrapper
+    """
+    wrangle(pisa_df, indep_groups, dep_group)
 
-#     select_columns_and_drop_nulls()
+    # df is edited in place, return is for pipeline
+    return pisa_df
 
-#     strings_to_known_types()
 
-#         get_categories()
+#%%% Wrangling Functions
 
-#         apply_preferred_values()
+def wrangle(pisa_df, indep_groups, dep_group):
+    """
+    wrangling wrapper
+    """
+    def select_columns_and_drop_nulls():
+        ### drop unused columns
+        ### drop nulls
+        pass
 
-#     return df
+    ### CAUTION:    large sets of variables will reduce the sample size
+    ### Reason:     each variable has its own set of nulls
+    ### Solution:   reduce variable sets after finding interactions
+    select_columns_and_drop_nulls()
+
+    # for pisa_group in (
+    #         list(independent_groups.values()) +
+    #         list(dependent_groups.values())):
+    #     strings_to_known_categories(pisa_group, df)
+
+    return pisa_df
+
+
+def strings_to_known_categories(pisa_set, pisa_df, known_cats, pref_cats):
+    """
+    Get variable type from of known_categories.
+    Apply formatting if value types of group memebers is not mismatched.
+    """
+    def apply_preferred_values(category_key):
+        """ \
+        update strings to preferred values (ex: "Yes" == True)
+        raise ValueError if incomplete preferred values found
+        """
+        # chek category_key member of preferred_values
+        # one column at a time, update to corresponding preferred values
+            # strip white space so values are uniform
+            # replace each known value
+        # confirm values are from preferred_naming, none overlooked
+        # if unique_values not a subset of preferred_values[key]:
+            # raise ValueError(var + ': incomplete preferred values.')
+
+    ### Use try and execpt to test for numeric types
+    # try convert int:
+    # except: try: convert float
+    # except: apply_preferred_values(get_category(pisa_set, pisa_df))
+
+    return pisa_df
+
+
+def get_category(pisa_set, pisa_df):
+    """
+    Determine if variables in pisa_set match a known categoy.
+    Determine if variables in pisa_set each have same category.
+    (Return category name) or (raise ValueError if mismatched).
+    """
+    category_key = "String"   # default to plain text
+
+    ### check first variable against known categories
+    # get and store unique values
+    # check if those unique values are a subset of a known category
+    ### if matched: category_key = known_key
+
+    ### iterate over group
+    # verify same category in each
+    ### if not: raise ValueError("category mismatch in set " + pisa_set)
+
+    return category_key
+
