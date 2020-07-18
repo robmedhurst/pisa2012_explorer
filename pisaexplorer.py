4cc8543ee045e33f79cde1fc4f9c1fa46433a6d0
Robbie <robert.medhurst241@gmail.com>
2020-07-18 19:35:40 -0400

    Rename global variables
    Functions cannot access directly, use function parameter

--- pisaexplorer.py
+++ pisaexplorer.py
@@ -142,15 +142,11 @@ def wrangle(pisa_df, inputs):
 
 #%% MAIN
 
-
-#%%% Load CSVs
-
-# ### raw csv data
-# PISA2012 = pd.read_csv(
-#     zipfile.ZipFile('pisa2012.csv.zip', 'r').open('pisa2012.csv'),
-#     sep=',', encoding='latin-1', error_bad_lines=False,
-#     dtype='unicode', index_col=False)
-
+### raw csv data
+PISA2012 = pd.read_csv(
+    zipfile.ZipFile('pisa2012.csv.zip', 'r').open('pisa2012.csv'),
+    sep=',', encoding='latin-1', error_bad_lines=False,
+    dtype='unicode', index_col=False)
 
 ### variable descriptions
 PISADICT2012 = pd.read_csv(
@@ -159,15 +155,12 @@ PISADICT2012 = pd.read_csv(
     dtype='unicode', index_col=False).rename(
         columns={'Unnamed: 0':'varname', 'x': 'description'})
 
-
-#%%% Load Terms
-
 ### Known Categories
 #
 # Dictionary containing lists. Each list contains the values of known
 # PISA variables. The key is a short string description of the category.
 #
-known_categories = {
+KNOWN_CATEGORIES = {
     'work_status':[
         'Working full-time <for pay>',
         'Working part-time <for pay>',
@@ -181,7 +174,7 @@ known_categories = {
 # Dictionary containing lists. Each list contains preferred values for
 # known category associated with key.
 #
-preferred_naming = {
+PREFERRED_NAMING = {
     'work_status':[
         'Full-time',
         'Part-time',
@@ -195,12 +188,12 @@ preferred_naming = {
 # Dictionary containing lists. Each list is a group of variable names.
 # The variables in a group must be of same type (float, y/, category X).
 #
-independent_groups = {
+INDEPENDENT_GROUPS = {
     'family_home': ['ST11Q01', 'ST11Q02', 'ST11Q03', 'ST11Q04', 'ST11Q05'],
     'parent_work': ['ST15Q01', 'ST19Q01'],
     'parent_isei': ['BFMJ2', 'BMMJ1', 'HISEI'],
     'HOMEPOS'    : ['HOMEPOS'],
-    'person_item': ['ST26Q02', 'ST26Q03', 'ST26Q08', 
+    'person_item': ['ST26Q02', 'ST26Q03', 'ST26Q08',
                     'ST26Q09', 'ST26Q10', 'ST26Q11']}
 
 ### Groups of Depenent Variables
@@ -208,7 +201,7 @@ independent_groups = {
 # Dictionary containing list. Lists are groups of variable names.
 # The variables must be numeric.
 #
-dependent_groups = {
+DEPENDENT_GROUPS = {
     'math_result': ['PV1MATH', 'PV2MATH', 'PV3MATH', 'PV4MATH', 'PV5MATH'],
     'read_result': ['PV1READ', 'PV2READ', 'PV3READ', 'PV4READ', 'PV5READ']
     }
@@ -216,5 +209,5 @@ dependent_groups = {
 
 temp_df = initialize(
     PISA2012.sample(500),
-    [known_categories, preferred_naming, 
-     independent_groups, dependent_groups])
+    [KNOWN_CATEGORIES, PREFERRED_NAMING,
+     INDEPENDENT_GROUPS, DEPENDENT_GROUPS])
