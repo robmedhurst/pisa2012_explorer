665aa330474eb96f6faee3a02750cfe4209210b7
Robbie <robert.medhurst241@gmail.com>
2020-07-18 19:27:50 -0400

    Reorganize wrangling functions
    initialize function takes all inputs as a paramater.
    All wrangling functions are subfuncions of wrangle function.
    Dictionary containing category group key pairs is returned by wangle.

--- pisaexplorer.py
+++ pisaexplorer.py
@@ -15,22 +15,23 @@ import seaborn as sns
 #%% FUNCTIONS
 
 
-def initialize(pisa_df):
+def initialize(pisa_df, inputs):
     """
     general wrapper
     """
-    wrangle(pisa_df)
-
-    # df is edited in place, return is for pipeline
-    return pisa_df
+    wrangle(pisa_df, inputs)
+    return pisa_df, inputs
 
 
 #%%% Wrangling Functions
 
-def wrangle(pisa_df):
+def wrangle(pisa_df, inputs):
     """
     wrangling wrapper
     """
+    (known_categories, preferred_naming,
+     independent_groups, dependent_groups) = inputs
+
     def select_columns_and_drop_nulls():
         """
         Remove columns from that are not in any given groups.
@@ -46,50 +47,7 @@ def wrangle(pisa_df):
         # drop nulls
         pisa_df.dropna(inplace=True)
 
-    ### CAUTION:    large sets of variables will reduce the sample size
-    ### Reason:     each variable has its own set of nulls
-    ### Solution:   reduce variable sets after finding interactions
-    select_columns_and_drop_nulls()
-
-    ### match variable type and update each
-    for pisa_group in (
-            list(independent_groups.values()) +
-            list(dependent_groups.values())):
-        strings_to_known_categories(pisa_group, pisa_df)
-
-    return pisa_df
-
-
-def strings_to_known_categories(pisa_group, pisa_df):
-    """
-    Get variable type from of known_categories.
-    Apply formatting if value types of group memebers is not mismatched.
-    """
-    def apply_preferred_values(category_key):
-        """
-        update strings to preferred values (ex: "Yes" == True)
-        raise ValueError if incomplete preferred values found
-        """
-        # ignore numerical and text_response types
-        if category_key in known_categories:
-            # update to corresponding preferred values
-            for var in pisa_group:
-
-                # strip white space to match know_categories
-                pisa_df[var] = pisa_df[var].map(lambda x: x.strip())
-
-                # replace each known value
-                for known, preferred  in zip(
-                        known_categories[category_key],
-                        preferred_naming[category_key]):
-                    pisa_df.loc[pisa_df[var] == known, var] = preferred
-
-                # confirm values are from preferred_naming, none overlooked
-                if not set(pisa_df[var].unique()).issubset(
-                        preferred_naming[category_key]):
-                    raise ValueError(var + ': incomplete preferred values.')
-
-    def get_category():
+    def get_category(pisa_group):
         """
         Determine if variables in pisa_group match a known categoy.
         Determine if variables in pisa_group each have same category.
@@ -115,25 +73,70 @@ def strings_to_known_categories(pisa_group, pisa_df):
             if not unique_values.issubset(known_categories[category_key]):
                 category_key = "text_response"
                 break
-
         return category_key
 
-    ### assume numeric and try to force conversion
-    numeric = True
-    try:
-        pisa_df[pisa_group] = pisa_df[pisa_group].astype(int)
-    except:
-        try:
-            pisa_df[pisa_group] = pisa_df[pisa_group].astype(float)
-        except:
-            # if conversion not possible, assume non numeric
-            numeric = False
-    if not numeric:
-        pisa_df[pisa_group] = pisa_df[pisa_group].astype(str)
-        # attempt to match and update values to known PISA category
-        apply_preferred_values(get_category(pisa_group, pisa_df))
+    def apply_preferred_values(pisa_group, category_key):
+        """
+        update strings to preferred values (ex: "Yes" == True)
+        raise ValueError if incomplete preferred values found
+        """
+        # ignore numerical and text_response types
+        if category_key in known_categories:
+            # update to corresponding preferred values
+            for var in pisa_group:
 
-    return pisa_df
+                # strip white space to match know_categories
+                pisa_df[var] = pisa_df[var].map(lambda x: x.strip())
+
+                # replace each known value
+                for known, preferred  in zip(
+                        known_categories[category_key],
+                        preferred_naming[category_key]):
+                    pisa_df.loc[pisa_df[var] == known, var] = preferred
+
+                # confirm values are from preferred_naming, none overlooked
+                if not set(pisa_df[var].unique()).issubset(
+                        preferred_naming[category_key]):
+                    raise ValueError(var + ': incomplete preferred values.')
+
+    def process_pisa_set_of_groups(pisa_set_of_groups):
+        """
+        Get category for each group,
+        set corresponding values for each group,
+        return dictionary containting found group/category key pairs.
+        """
+        group_category_matches = {}
+        for group_key in pisa_set_of_groups:
+            pisa_group = pisa_set_of_groups[group_key]
+
+            # attempt to convert group to numeric
+            try:
+                pisa_df[pisa_group] = pisa_df[pisa_group].astype(int)
+                category = "integer"
+            except:
+                try:
+                    pisa_df[pisa_group] = pisa_df[pisa_group].astype(float)
+                    category = "float"
+                except:
+                    category = None
+            if not category:
+                pisa_df[pisa_group] = pisa_df[pisa_group].astype(str)
+                
+                # attempt to match and update values to known PISA category
+                category = get_category(pisa_group)
+                apply_preferred_values(pisa_group, category)
+            group_category_matches[group_key]=category
+        return group_category_matches
+
+    ### CAUTION:    large sets of variables will reduce the sample size
+    ### Reason:     each variable has its own set of nulls
+    ### Solution:   reduce variable sets after finding interactions
+    select_columns_and_drop_nulls()
+    # independent_groups_keys = process_pisa_set_of_groups(independent_groups)
+    group_category_matches = process_pisa_set_of_groups(
+        {**independent_groups, **dependent_groups})
+
+    return group_category_matches
 
 
 
@@ -142,11 +145,11 @@ def strings_to_known_categories(pisa_group, pisa_df):
 
 #%%% Load CSVs
 
-### raw csv data
-PISA2012 = pd.read_csv(
-    zipfile.ZipFile('pisa2012.csv.zip', 'r').open('pisa2012.csv'),
-    sep=',', encoding='latin-1', error_bad_lines=False,
-    dtype='unicode', index_col=False)
+# ### raw csv data
+# PISA2012 = pd.read_csv(
+#     zipfile.ZipFile('pisa2012.csv.zip', 'r').open('pisa2012.csv'),
+#     sep=',', encoding='latin-1', error_bad_lines=False,
+#     dtype='unicode', index_col=False)
 
 
 ### variable descriptions
@@ -211,8 +214,7 @@ dependent_groups = {
     }
 
 
-sample_size = 500
-
-pisa_sample = PISA2012.sample(sample_size)
-
-initialize(pisa_sample)
+temp_df = initialize(
+    PISA2012.sample(500),
+    [known_categories, preferred_naming, 
+     independent_groups, dependent_groups])
