3790a31ea183396c5f16e07e0d44707339f96b5c
Robbie <robert.medhurst241@gmail.com>
2020-07-19 23:49:26 -0400

    Create new file category_specific.py
    File holds functions to be called on category groups.
    Placeholder functions created.

---
+++ category_specific.py
@@ -0,0 +1,27 @@
+def integer_group_post_wrangle(group_name, pisa_df, inputs):
+    """
+    apply integer group operations:
+        - 
+    """
+    pass
+
+def float_group_post_wrangle(group_name, pisa_df, inputs):
+    """
+    apply float group operations:
+        - create mean value column
+    """
+    pass
+
+def binary_yn_group_post_wrangle(group_name, pisa_df, inputs):
+    """
+    apply binary_yn group operations:
+        - create mean value column
+    """
+    pass
+
+def work_status_group_post_wrangle(group_name, pisa_df, inputs):
+    """
+    apply work_status group operations:
+        - create mean value column
+    """
+    pass
