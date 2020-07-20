e38caa5c2f4ec942ce97a754c74eaa2ca1d3e951
Robbie <robert.medhurst241@gmail.com>
2020-07-20 10:22:22 -0400

    Add function binary_yn_group_post_wrangle
    Adding function binary_yn_group_post_wrangle to category_specific.py
    to handle categorical group post wrangling processing.

--- category_specific.py
+++ category_specific.py
@@ -1,10 +1,14 @@
+# handle independent and dependent variables seperately
+
+### integer
 def integer_group_post_wrangle(group_name, pisa_df, inputs):
     """
     apply integer group operations:
-        - 
+        - None
     """
     pass
 
+### float
 def float_group_post_wrangle(group_name, pisa_df, inputs):
     """
     apply float group operations:
@@ -12,16 +16,26 @@ def float_group_post_wrangle(group_name, pisa_df, inputs):
     """
     pass
 
+### binary_yn
 def binary_yn_group_post_wrangle(group_name, pisa_df, inputs):
     """
     apply binary_yn group operations:
-        - create mean value column
+        - create total column for binary group given
     """
-    pass
+    independent_groups, dependent_groups = inputs[2:4]
 
+    def apply_count(nm, grp):
+        pisa_df[nm + 'Count'] = pisa_df[grp].transpose().sum().astype(int)
+
+    if group_name in independent_groups:
+        apply_count(group_name, independent_groups[group_name])
+    elif group_name in dependent_groups:
+        apply_count(group_name, dependent_groups[group_name])
+
+### work_status
 def work_status_group_post_wrangle(group_name, pisa_df, inputs):
     """
     apply work_status group operations:
-        - create mean value column
+        - None
     """
     pass
