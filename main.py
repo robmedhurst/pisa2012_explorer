5aa57583bd778a727dc2bdc62d9fe679891e9c27
Robbie <robert.medhurst241@gmail.com>
2020-07-22 16:29:45 -0400

    Add temorary helper function completeness_check
    completeness_check is a temporary function that allows checking the
    category found for each PISA variable.

--- main.py
+++ main.py
@@ -47,25 +47,57 @@ def group_post_wrangle(pisa_df, inputs, group_category_matches):
     apply category specific actions for each group
     raise ValueError if no corresponding function found
     """
-    # iterate group category matches
-    for group_name in group_category_matches:
-        category = group_category_matches[group_name]
+    # group_category_matches holds indep and dependent groups seperately
+    for subset in group_category_matches:
+        # iterate group category matches
+        for group_name in group_category_matches[subset]:
+            category = group_category_matches[subset][group_name]
+            # check if associated post wrangling group actions are available
+            if (category + "_group_post_wrangle") in dir(category_functions):
+    
+                # function call using getattr
+                getattr(
+                    category_functions, (category + "_group_post_wrangle"))(
+                        group_name, pisa_df, inputs)
+            else:
+                raise ValueError(
+                    "No post wrangle funcion found for group: '" + \
+                        group_category_matches[subset][group_name])
 
-        # check if associated post wrangling group actions are available
-        if (category + "_group_post_wrangle") in dir(category_functions):
-
-            # function call using getattr
-            getattr(category_functions, (category + "_group_post_wrangle"))(
-                group_name, pisa_df, inputs)
-        else:
-            raise ValueError(
-                "No post wrangle funcion found for group: '" + \
-                    group_category_matches[group_name])
     return pisa_df, inputs, group_category_matches
 
 returned_from_initialize = initialize(
     PISA2012.sample(500),
     [category_definitions.KNOWN_CATEGORIES,
-     category_definitions.PREFERRED_NAMING,
-     test_groupings.INDEP_test_grouping02,
-     test_groupings.DEPEN_test_grouping02])
+      category_definitions.PREFERRED_NAMING,
+      test_groupings.INDEP_test_grouping02,
+      test_groupings.DEPEN_test_grouping02])
+
+
+
+
+# a temporary helper function to check each column against known categories
+def completeness_check(column_start, column_end, interest_type=None):
+    
+    check = {}
+    for var in PISA2012.columns[column_start:column_end]:
+        check[str(var)] = [str(var)]
+
+    completeness = initialize(
+        PISA2012.sample(500),
+        [category_definitions.KNOWN_CATEGORIES,
+          category_definitions.PREFERRED_NAMING,
+          check,
+          test_groupings.DEPEN_test_grouping01])[2]['indep_categories']
+
+    if interest_type:
+        for key in completeness:
+            if completeness!= interest_type:
+                print(key)
+
+    return completeness
+
+# check 100 at a time
+completeness_check(0, 100)
+
+
