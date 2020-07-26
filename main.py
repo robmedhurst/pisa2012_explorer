6a2e99109ae3e4c44d5a571c0326a61011ac0d39
Robbie <robert.medhurst241@gmail.com>
2020-07-26 10:48:58 -0400

    Readd two placeholder functions
    text_response type and integer type are very common.
    Stops throwing warning for types that should be ignored.

--- main.py
+++ main.py
@@ -22,27 +22,34 @@ import test_groupings
 # Dataset can take a few minutes to load on some systems.
 # Load once and only work on copy.
 # Try to laod from csv then from zip.
-def load_original(reload=True, integrity_check=False):
+def load_original(reload=False, integrity_check=False):
     """
     Load original PISA2012 dataset from file and return it as an DataFrame.
     Does not load if already in memory.
-    Forces reload on parameter reload = True.
+    Forces reload on parameter 'reload=True'.
+    Checks datastructure if parameter 'integrety_check=True'.
     """
-    def pisa_confirmation(pisa_df):
+    def confirm_pisa_df():
         """
-        Raise error if original csv or zip file being used fails hash check
+        Return DataFrame pisa_df if no error found or if no check requested.
         """
-        print("Checking file integrity(this may take a few minutes)...\n")
-
-        # TODO: implement integrity check
-        passed = True
-
-        # return if test passed, raise error if failed
-        if passed:
-            print(" Dataframe passed integrity check.\n")
-        else:
+        if integrity_check:
+            print("Checking file integrity(this may take a few minutes)...\n")
+            # TODO: implement integrity check
+            #
+            # do the check here
+            #
+            #
+            #
+            #
+            #
+            passed = True    # result of check
+            # return if test passed, raise error if failed
+            if passed:
+                print("Dataframe passed integrity check.\n")
+                return pisa_df
             raise FileExistsError("Datafrane failed integrity check!")
-
+        return pisa_df
     if 'PISA2012' not in globals():
         print("PISA2012 original not in locals, attempting to load",
               "(this may take a few minutes)...\n")
@@ -51,7 +58,8 @@ def load_original(reload=True, integrity_check=False):
               "(this may take a few minutes)...\n")
     else:
         print("Variable with name PISA2012 already in memory.\n")
-
+        pisa_df = PISA2012
+        return confirm_pisa_df()
     # load, check, raise error if needed
     if ('PISA2012' not in globals()) or (reload):
         # global PISA2012
@@ -60,6 +68,7 @@ def load_original(reload=True, integrity_check=False):
                 'pisa2012.csv', sep=',', encoding='latin-1',
                 error_bad_lines=False, dtype='unicode', index_col=False)
             print("Loaded from csv.\n")
+            return confirm_pisa_df()
         except FileNotFoundError:
             try:    # loading directly from zip
                 pisa_df = pd.read_csv(
@@ -68,11 +77,9 @@ def load_original(reload=True, integrity_check=False):
                     sep=',', encoding='latin-1', error_bad_lines=False,
                     dtype='unicode', index_col=False)
                 print("Loaded from zip.\n")
+                return confirm_pisa_df()
             except FileNotFoundError:    # loading failed
                 raise FileNotFoundError("PISA2012 not in local directory.")
-    if integrity_check:
-        pisa_confirmation(pisa_df)
-    return pisa_df
 
 def initialize(pisa_df, inputs):
     """
@@ -114,9 +121,9 @@ def group_post_wrangle(pisa_df, inputs, group_category_matches):
                         group_name, pisa_df, inputs)
             # print warning if no category found
             else:
-                warnings.warn(
-                    "No post wrangle funcion found for group: '" +
-                    group_name + "    of category: " + category + "\n")
+                message = "No post wrangle funcion found for group: '" + \
+                    group_name + "', category: '" + category + "'"
+                warnings.warn(message)
     return pisa_df, inputs, group_category_matches
 
 
@@ -128,3 +135,8 @@ RETURNED_FROM_INITIALIZE = initialize(
      category_definitions.PREFERRED_NAMING,
      test_groupings.INDEP_test_grouping02,
      test_groupings.DEPEN_test_grouping02])
+
+
+# temporary testing vars, will delete before merging:
+# COMPLETENESS_CHECK = test_groupings.completeness_check(PISA2012, 0, 20, None)
+# ALL_SHORT_UNIQUES = test_groupings.get_all_unique_short_categories(PISA2012, 20)
