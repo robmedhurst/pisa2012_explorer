3a65162a3fd3886096a5e3dc16853d20f7fda1c4
Robbie <robert.medhurst241@gmail.com>
2020-08-06 12:39:59 -0400

    Add file bivariate_graphics_pool.py
    Create file and placeholder function.
    Searchable bivariate plot functions will live here.

---
+++ bivariate_graphics_pool.py
@@ -0,0 +1,10 @@
+"""
+Functions for bivariate group plotting.
+
+Functions for bivariate group plotting with verbose names to be
+caught by keyword.
+"""
+
+
+def horizontalcomparion_violin_catfloat():
+    """Placeholer function."""
