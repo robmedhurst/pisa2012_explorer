5899160d6e4a7856b4ba7e66bd396311db9e4174
Robbie <robert.medhurst241@gmail.com>
2020-08-13 16:27:43 -0400

    Add function pickle_buffer
    Simply pickle, buffer, and return and object. Saves repeated code.
    
    Also move to function countplot_uni_row_cat.

--- graphics_.py
+++ graphics_.py
@@ -6,8 +6,17 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import main_definitions
+import main_definitions as definitions
 from main_ import get_longnames
+
+
+def pickle_buffer(fig):
+    """Given an object, return pickled version as io.BytesIO object."""
+    buf = io.BytesIO()
+    output = buf
+    pickle.dump(fig, output)
+    plt.close(fig)
+    return buf
 
 
 def violinplot_bi_row_float_cat(parameters, pisa_df, inputs):
@@ -60,11 +69,7 @@ def violinplot_bi_row_float_cat(parameters, pisa_df, inputs):
                     ylabel=yname
                     )
 
-    buf = io.BytesIO()
-    output = buf
-    pickle.dump(fig, output)
-    plt.close(fig)
-    return buf
+    return pickle_buffer(fig)
 
 
 def barplot_uni_single_binary(group_info, user_data):
@@ -84,11 +89,7 @@ def barplot_uni_single_binary(group_info, user_data):
     plt.xticks(rotation=30, ha='right')
     plt.ylabel('count')
 
-    buf = io.BytesIO()
-    output = buf
-    pickle.dump(fig, output)
-    plt.close(fig)
-    return buf
+    return pickle_buffer(fig)
 
 
 def distplot_uni_row_float_nokde(group_info, user_data):
@@ -103,7 +104,62 @@ def distplot_uni_row_float_kde(group_info, user_data):
 
 def countplot_uni_row_cat(group_info, user_data):
     """."""
-    return float_horizontal_frequency(group_info, user_data, "categorical")
+    pisa_df = user_data['custom_dataframe']
+    var_list = group_info['variables']
+    category = group_info['category']
+    max_ylim = 0
+
+    for first_pass in [True, False]:
+        # initialize figure
+        # width 5*n_vars, height 5, force close to 1:1 aspect ratio
+        if first_pass:
+            fig = plt.figure(figsize=(5 * len(var_list), 5))
+        else:
+            # discard first draft
+            plt.close(fig)
+            del fig
+            fig = plt.figure(figsize=(5 * len(var_list), 5))
+
+        # for each var create axis and titles
+        for var_index, var_name in enumerate(var_list):
+            # add a subplot for each var_name in var_list
+            axis = fig.add_subplot(1, len(var_list), var_index + 1)
+
+            sns.countplot(
+                pisa_df[var_name],
+                order=definitions.PREFERRED_NAMING[category],
+                color=sns.color_palette()[0]
+                )
+
+            # Get plot information (max y limit) on first pass
+            if first_pass:
+                axis = plt.gca()
+                new_ylim = axis.get_ylim()[1]
+                if new_ylim > max_ylim:
+                    max_ylim = new_ylim
+
+            # Apply formatting on second pass
+            else:
+                # only display y label on first subplot
+                if var_index == 0:
+                    yname = "Count"
+                else:
+                    yname = None
+
+                # use long names on x axis if available
+                xname = str(var_list[var_index])
+                if get_longnames([var_list[var_index]]):
+                    xname = xname + ': ' + str(
+                        get_longnames([var_list[var_index]])[0])
+
+                # set axis limits and titles
+                axis.set(
+                    ylim=(0, max_ylim),
+                    xlabel=xname,
+                    ylabel=yname
+                    )
+
+    return pickle_buffer(fig)
 
 
 def float_horizontal_frequency(group_info, user_data, switcher):
@@ -122,7 +178,7 @@ def float_horizontal_frequency(group_info, user_data, switcher):
 
     elif switcher == "categorical":
         first_y_name = "Count"
-        switcher = main_definitions.PREFERRED_NAMING[category]
+        switcher = definitions.PREFERRED_NAMING[category]
 
     for first_pass in [True, False]:
         # initialize figure
@@ -185,8 +241,4 @@ def float_horizontal_frequency(group_info, user_data, switcher):
                     ylabel=yname
                     )
 
-    buf = io.BytesIO()
-    output = buf
-    pickle.dump(fig, output)
-    plt.close(fig)
-    return buf
+    return pickle_buffer(fig)
