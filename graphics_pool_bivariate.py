bcad407bd17499cc196726bfffbe3254f6bfac5e
Robbie <robert.medhurst241@gmail.com>
2020-08-18 12:18:27 -0400

    Add function boxplot_bi_row_cat to graphics_.py
    This is a copy of violinplot_bi_row_cat.
    A wrapper for these functions will be used to reduce
    overcomplicated code while still highlighting which seaborn
    function is called.

--- graphics_pool_bivariate.py
+++ graphics_pool_bivariate.py
@@ -8,6 +8,11 @@ caught by keyword.
 import graphics_ as gs
 
 
-def violinplot_bi_row_cat(group_info, user_data):
-    """Return a subplot of distributions for float type varibles."""
-    return gs.violinplot_bi_row_cat(group_info, user_data)
+def violinplot_bi_row_cat(response_info, user_data):
+    """."""
+    return gs.violinplot_bi_row_cat(response_info, user_data)
+
+
+def boxplot_bi_row_cat(response_info, user_data):
+    """."""
+    return gs.boxplot_bi_row_cat(response_info, user_data)
