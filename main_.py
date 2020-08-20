9e29a39ae67a34d203bde281a16bea989e9e6071
robmedhurst <robert.medhurst241@gmail.com>
2020-08-20 17:02:31 -0400

    Rewrite user_single_variable_graphics

--- main_.py
+++ main_.py
@@ -95,227 +95,77 @@ def user_initialize(parameter_input=None):
 
     Returns pisa_sample, inputs
     """
-    def apply_preset(preset):
-        try:
-            sample_in = preset['pisa_sample']
+    def activate_preset(user_data):
+        def do_pisa_sample(user_data):
+            sample_in = user_data['pisa_sample']
             if isinstance(sample_in, int):
-                preset['sample_size'] = sample_in
-                preset['pisa_sample'] = pisa2012.sample(sample_in)
-        except KeyError:
-            return "KeyError on user_initialize 'preset'"
-        return preset
+                user_data['sample_size'] = sample_in
+                user_data['pisa_sample'] = pisa2012.sample(sample_in)
+            return user_data
 
-    def build_new_user_data():
+        def do_response_tracker_key(user_data):
+            try:
+                user_data['response_trackers']
+            except KeyError:
+                user_data['response_trackers'] = {}
+            return user_data
+        return do_response_tracker_key(do_pisa_sample(user_data))
+
+    def initialize_using_parameter():
+        if parameter_input is not None:
+            print("Skipping initialize, loaded selection from parameter.\n")
+            return activate_preset(parameter_input)
+
+    def initialize_using_output():
+        if 'OUTPUT' in globals():
+            print("Would you like to reuse existing OUTPUT?")
+            if ui.single_response_from_list(['yes', 'no']) == 'yes':
+                return activate_preset(OUTPUT.copy())
+
+    def initialize_using_preset():
+        print("Use preset? ('no' to input sample size and groups)")
+        if ui.single_response_from_list(['yes', 'no']) == 'yes':
+            return activate_preset(PRESET1.copy())
+
+    def initialize_using_user_input():
         built_data = {}
         built_data['dependent_groups'] = (
             ui.user_select_dependent_group(list(pisa2012.columns)))
         built_data['independent_groups'] = (
             ui.user_select_independent_groups(list(pisa2012.columns)))
         built_data['sample_size'] = ui.user_set_sample_size()
-        built_data['pisa_sample'] = pisa2012.sample(built_data['sample_size'])
-        return built_data
+        return activate_preset(built_data)
 
     def do_build_user_data():
-        # check for paramater input and existing gloabl OUTPUT
-        if parameter_input is not None:
-            print("Skipping initialize, loaded selection from parameter.\n")
-            return apply_preset(parameter_input)
-        if 'OUTPUT' in globals():
-            print("Would you like to reuse existing OUTPUT?")
-            if ui.single_response_from_list(['yes', 'no']) == 'yes':
-                return apply_preset(OUTPUT.copy())
-        # user initiated preset
-        print("\n")
-        print("Use preset? ('no' to input sample size and groups)")
-        if ui.single_response_from_list(['yes', 'no']) == 'yes':
-            print("Using preset...")
-            return apply_preset(PRESET1.copy())
-        # user create new user_data
-        return build_new_user_data()
+        user_data = initialize_using_parameter()
+        if not user_data:
+            user_data = initialize_using_output()
+        if not user_data:
+            user_data = initialize_using_preset()
+        if not user_data:
+            user_data = initialize_using_user_input()
+        return user_data
 
-    # load original data
     try:
         pisa2012 = PISA2012.copy()
-    # fall back to reload from csv, then zip
     except NameError:
         pisa2012 = load_original_from_file()
-
-    user_data = do_build_user_data()
-    print(user_data)
-    return user_data
+    return do_build_user_data()
 
 
 # %%% user_request_singlevar_graphics
-
 def user_single_variable_graphics(user_data):
-    """User select plots."""
-    # ========================================================================
-    #
-    group_category_matches = user_data['group_category_matches']
-
-    # private=================================================================
-    #
-    def user_select_groups(list_of_groups):
-        """User choose any number from list of groups."""
-        if len(list_of_groups) > 1:
-            print("\n")
-            print("Which groups to use...")
-            print("Use all groups?")
-            if ui.single_response_from_list(['yes', 'no']) == 'yes':
-                return list_of_groups
-            print("Which groups to use...")
-            return ui.multi_responses_from_list(list_of_groups)
-        return list_of_groups
-
-    def get_singlevar_group_functions(group_info):
-        # get functions matching category key from univatiate pool
-        list_of_functions = ui.get_function_by_key(
-            group_info['category'], 'singlevariable')
-        # Detect BINARY
-        if group_info['category'] in KNOWN_CATEGORIES and len(
-                KNOWN_CATEGORIES[group_info['category']]) == 2:
-            # get binary functions
-            list_of_functions.extend(
-                ui.get_function_by_key('binary', 'singlevariable'))
-        # Detect CATEGORICAL
-        if group_info['category'] in KNOWN_CATEGORIES:
-            # get binary functions
-            list_of_functions.extend(
-                ui.get_function_by_key('cat', 'singlevariable'))
-        return list_of_functions
-
-    def user_select_functions(group_info):
-        list_of_functions = get_singlevar_group_functions(group_info)
-        # bypass user input
-        if response_tracker['bypass'] == 'none':
-            return []
-        elif response_tracker['bypass'] == 'all':
-            return list_of_functions
-        # different user prompts for different number of available functions
-        elif len(list_of_functions) == 1:
-            print("\n")
-            print("Do you want to use function",
-                  list_of_functions[0], "for group", group_info['name'], "?")
-            if ui.single_response_from_list(['yes', 'no']) == 'no':
-                return []
-        elif len(list_of_functions) > 1:
-            print("\n")
-            print("Use all functions for group", group_info['name'], "?")
-            if ui.single_response_from_list(['yes', 'no']) == 'no':
-                print("\n")
-                print("Which functions to use for group",
-                      group_info['name'], "?")
-                return ui.multi_responses_from_list(list_of_functions)
-        return list_of_functions
-
-    def get_singlevar_graphic(function_name, group_info):
-        return ui.get_single_graphic(
-            'singlevariable', function_name, (group_info, user_data))
-
-    def iterate_group_function_selection(location):
-        groups_to_check = response_tracker[location]
-        graphics_by_group = {}
-        for group_name in groups_to_check:
-            # define group
-            graphics_by_group[group_name] = {}
-            group_info = {
-                'name': group_name,
-                'variables': user_data[location][group_name],
-                'category': group_category_matches[location][group_name]}
-            # interact with user
-            response_tracker['functions'][group_name] = (
-                user_select_functions(group_info))
-            # call selected functions
-            for function_name in response_tracker['functions'][group_name]:
-                graphics_by_group[group_name][function_name] = (
-                    get_singlevar_graphic(function_name, group_info))
-        return graphics_by_group
-
-    def init_response_tracker(bypass_type):
-        if bypass_type == "Yes, with all plots.":
-            return {
-                # populate with responses indicating ALL options selected
-                'bypass': 'all',
-                'dependent_groups':
-                    list(group_category_matches['dependent_groups'].keys()),
-                'independent_groups':
-                    list(group_category_matches['independent_groups'].keys()),
-                'functions': {}}
-        elif bypass_type == "Yes, with no plots.":
-            return {
-                # populate with responses indicating NO options selected
-                'bypass': 'none',
-                'independent_groups': [],
-                'dependent_groups': []}
-        else:
-            return {'bypass': False}
-
-    def user_select_bypass():
-        # User Select Bypass
-        print("Bypass selection?")
-        return ui.single_response_from_list([
-            "Yes, with all plots.",
-            "Yes, with no plots.",
-            "No, manually select groups to explore."
-            ])
-
+    """."""
     print("\n\n")
     print("Choose Single Variable Graphics")
-    try:
-        response_tracker = user_data['response_trackers']['singlevariable']
-    except KeyError:
-        response_tracker = False
-    #
-    # response_tracker old
-    if response_tracker is not False:
-        print("Existing responses found, reuse them?")
-        if ui.single_response_from_list(["yes", "no"]) == 'no':
-            # move the old tracker
-            user_data['response_trackers'][
-                ui.get_next_unused_name(
-                    user_data,
-                    ['response_trackers'], 'singlevariable'
-                    )] = response_tracker
-            response_tracker = False
-    # response_tracker new
-    if response_tracker is False:
-        response_tracker = init_response_tracker(user_select_bypass())
-    # storage
-    singlevar_graphic_objects = {
-        'dependent_groups': {},
-        'independent_groups': {}
-        }
-    #
-    # Dependent Groups
-    print("\n")
-    print("Dependent Variables")
-    # group selection
-    if not response_tracker['bypass']:  # user select groups if no bypass
-        response_tracker['dependent_groups'] = user_select_groups(list(
-            group_category_matches['dependent_groups'].keys()))
-    # function selection
-    singlevar_graphic_objects['dependent_groups'] = (
-        iterate_group_function_selection('dependent_groups'))
-    #
-    # Independent Groups
-    print("\n")
-    print("Independent Variables")
-    # group selection
-    if not response_tracker['bypass']:  # user select groups if no bypass
-        response_tracker['independent_groups'] = user_select_groups(list(
-            group_category_matches['independent_groups'].keys()))
-    # function selection
-    singlevar_graphic_objects['independent_groups'] = (
-        iterate_group_function_selection('independent_groups'))
-    #
-    # interaction end ========================================================
-    # ========================================================================
-    #
-    # Update and Return user_data
-    if 'response_trackers' not in user_data.keys():
-        user_data['response_trackers'] = {}
+    # INPUTS
+    response_tracker = ui.initialize_tracker(user_data, 'singlevariable')
+    # GRAPHICS
+    graphic_buffer_objects = ui.graphics_from_responses(
+        response_tracker, user_data, 'singlevariable')
+    # UPDATES
     user_data['response_trackers']['singlevariable'] = response_tracker
-    user_data['singlevariable_graphic_objects'] = singlevar_graphic_objects
+    user_data['singlevariable_graphic_objects'] = graphic_buffer_objects
     return user_data
 
 
@@ -367,8 +217,8 @@ def show_all_output(result):
                 if graphic is not None:
                     graphic.seek(0)
                     pickle.load(graphic)
-    rip_lanes(['singlevariable_graphic_objects', 'dependent_groups'])
-    rip_lanes(['singlevariable_graphic_objects', 'independent_groups'])
+    rip_lanes(['singlevariable_graphic_objects'])
+    rip_lanes(['singlevariable_graphic_objects'])
     rip_lanes(['univariate_graphic_objects'])
 
 
