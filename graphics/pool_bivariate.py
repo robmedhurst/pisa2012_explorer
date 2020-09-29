18c38196328da38ace14b418dcafe1704157b507
robmedhurst <robert.medhurst241@gmail.com>
2020-09-29 11:21:33 -0400

    Create plot funtion pairplot_hist_diag_scatter
    Signed-off-by: robmedhurst <robert.medhurst241@gmail.com>

--- graphics/pool_bivariate.py
+++ graphics/pool_bivariate.py
@@ -7,6 +7,21 @@ caught by keyword.
 
 import graphics.graphics as gs
 
+# BIVARIATE GRAPHICS POOL
+
+
+def pairplot_hist_diag_scatter(response, user_data):
+    """Return a pair grid of point plots."""
+    if 'functions' in response:
+        return gs.pairplot_hist_diag_scatter(response, user_data)
+    else:
+        # do checks
+        dep, indep, categories = gs.concise_reponse_info(response)
+        return (
+            indep[0]['category'] == "float" and
+            indep[1]['category'] == "float"
+            )
+
 
 def countplot_bi_grid_1cat_2cat(response, user_data):
     """Return a grid of countplots."""
